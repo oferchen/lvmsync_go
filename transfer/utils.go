71b5832a68a5791f8fdf0ec4b873fc2bea92c2bf
Ofer Chen <oferchen@gmail.com>
2025-03-02 16:18:56 +0100

    Avoids redundant rate limiter creation, Returns original writer when no speed limit is set, Improved performance in repeated calls

--- transfer/utils.go
+++ transfer/utils.go
@@ -3,14 +3,31 @@ package transfer
 
 import (
 	"io"
+	"sync"
 
 	"github.com/juju/ratelimit"
 )
 
+var (
+	rateLimiterCache     *ratelimit.Bucket
+	lastSpeedLimit       int
+	rateLimiterCacheLock sync.Mutex
+)
+
 func WrapRateLimitedWriter(w io.Writer, speedLimit int) io.Writer {
-	if speedLimit > 0 {
-		bucket := ratelimit.NewBucketWithRate(float64(speedLimit), int64(speedLimit))
-		return ratelimit.Writer(w, bucket)
+	if speedLimit <= 0 {
+		return w
 	}
-	return w
+
+	rateLimiterCacheLock.Lock()
+	defer rateLimiterCacheLock.Unlock()
+
+	if rateLimiterCache != nil && lastSpeedLimit == speedLimit {
+		return ratelimit.Writer(w, rateLimiterCache)
+	}
+
+	rateLimiterCache = ratelimit.NewBucketWithRate(float64(speedLimit), int64(speedLimit))
+	lastSpeedLimit = speedLimit
+
+	return ratelimit.Writer(w, rateLimiterCache)
 }
