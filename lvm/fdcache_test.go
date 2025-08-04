585950400667f95a51fec498881ff6c1dd122dd2
Ofer Chen <oferchen@gmail.com>
2025-08-04 12:51:50 +0200

    Implement LRU fd cache

---
+++ lvm/fdcache_test.go
@@ -0,0 +1,86 @@
+package lvm
+
+import (
+	"container/list"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"golang.org/x/sys/unix"
+)
+
+func TestFdCacheEvictionOrder(t *testing.T) {
+	cache := &fdCache{
+		fds:   make(map[string]*list.Element),
+		order: list.New(),
+	}
+
+	tmpDir := t.TempDir()
+	var fd0, fd1 int
+	for i := 0; i < fdCacheSize; i++ {
+		path := filepath.Join(tmpDir, fmt.Sprintf("file%d", i))
+		if err := os.WriteFile(path, nil, 0644); err != nil {
+			t.Fatalf("write file %d: %v", i, err)
+		}
+		fd, err := cache.getFD(path)
+		if err != nil {
+			t.Fatalf("getFD %d: %v", i, err)
+		}
+		if i == 0 {
+			fd0 = fd
+		}
+		if i == 1 {
+			fd1 = fd
+		}
+	}
+
+	if _, err := cache.getFD(filepath.Join(tmpDir, "file0")); err != nil {
+		t.Fatalf("reaccess file0: %v", err)
+	}
+
+	extra := filepath.Join(tmpDir, "extra")
+	if err := os.WriteFile(extra, nil, 0644); err != nil {
+		t.Fatalf("write extra: %v", err)
+	}
+	if _, err := cache.getFD(extra); err != nil {
+		t.Fatalf("getFD extra: %v", err)
+	}
+
+	if _, err := unix.FcntlInt(uintptr(fd1), unix.F_GETFD, 0); err == nil {
+		t.Fatalf("expected fd1 to be closed")
+	}
+	if _, err := unix.FcntlInt(uintptr(fd0), unix.F_GETFD, 0); err != nil {
+		t.Fatalf("expected fd0 to remain open: %v", err)
+	}
+
+	cache.Close()
+}
+
+func TestFdCacheCloseClosesAll(t *testing.T) {
+	cache := &fdCache{
+		fds:   make(map[string]*list.Element),
+		order: list.New(),
+	}
+	tmpDir := t.TempDir()
+	var fds []int
+	for i := 0; i < 3; i++ {
+		path := filepath.Join(tmpDir, fmt.Sprintf("file%d", i))
+		if err := os.WriteFile(path, nil, 0644); err != nil {
+			t.Fatalf("write file %d: %v", i, err)
+		}
+		fd, err := cache.getFD(path)
+		if err != nil {
+			t.Fatalf("getFD %d: %v", i, err)
+		}
+		fds = append(fds, fd)
+	}
+
+	cache.Close()
+
+	for i, fd := range fds {
+		if _, err := unix.FcntlInt(uintptr(fd), unix.F_GETFD, 0); err == nil {
+			t.Fatalf("fd %d still open", i)
+		}
+	}
+}
