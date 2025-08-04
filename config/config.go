a2ebef339c31c8d42e79da90f7a16d3d38a31209
Ofer Chen <oferchen@gmail.com>
2025-08-05 01:55:44 +0200

    condition compression level check (#94)

--- config/config.go
+++ config/config.go
@@ -97,9 +97,11 @@ func (cb *ConfigBuilder) Build() (*Config, error) {
 	}
 	conf.SpeedLimit = sl
 
-	// Validate compression level for zstd.
-	if conf.CompressLevel < 1 || conf.CompressLevel > 22 {
-		return nil, fmt.Errorf("invalid zstd compression level: %d", conf.CompressLevel)
+	// Validate compression level for zstd or auto.
+	if conf.Compress == "zstd" || conf.Compress == "auto" {
+		if conf.CompressLevel < 1 || conf.CompressLevel > 22 {
+			return nil, fmt.Errorf("invalid zstd compression level: %d", conf.CompressLevel)
+		}
 	}
 
 	return &conf, nil
