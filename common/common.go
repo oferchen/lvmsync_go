ea1fa1d59ca7d10bc76fa28c912ceec428ae6581
Ofer Chen <oferchen@gmail.com>
2025-08-07 10:36:45 +0200

    docs: clarify protocol version (#187)

--- common/common.go
+++ common/common.go
@@ -1,4 +1,7 @@
 // common/common.go
 package common //nolint:revive // package name is established API
 
+// ProtocolVersion identifies the string exchanged during the initial handshake.
+// It follows the format "lvmsync PROTO[<n>]" where <n> is the protocol version
+// number both peers must support.
 const ProtocolVersion = "lvmsync PROTO[3]"
