cb43ade49993dcc44fe7cbb69939080cf42bad87
Ofer Chen <oferchen@gmail.com>
2025-08-07 15:23:40 +0200

    Return explicit error when CGO unsupported

---
+++ lvm/cgo/devmapper_stub_test.go
@@ -0,0 +1,28 @@
+//go:build !linux || !cgo
+
+package cgo
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestStubReturnsErrUnsupported(t *testing.T) {
+	dm := New()
+
+	if err := dm.CreateSnapshot("lv", "snap", 1); !errors.Is(err, ErrUnsupported) {
+		t.Fatalf("CreateSnapshot error = %v, want ErrUnsupported", err)
+	}
+	if err := dm.RemoveLV("lv"); !errors.Is(err, ErrUnsupported) {
+		t.Fatalf("RemoveLV error = %v, want ErrUnsupported", err)
+	}
+	if _, err := dm.SnapshotUsage("lv"); !errors.Is(err, ErrUnsupported) {
+		t.Fatalf("SnapshotUsage error = %v, want ErrUnsupported", err)
+	}
+	if _, err := dm.VGFree("vg"); !errors.Is(err, ErrUnsupported) {
+		t.Fatalf("VGFree error = %v, want ErrUnsupported", err)
+	}
+	if _, err := dm.ListVGs(); !errors.Is(err, ErrUnsupported) {
+		t.Fatalf("ListVGs error = %v, want ErrUnsupported", err)
+	}
+}
