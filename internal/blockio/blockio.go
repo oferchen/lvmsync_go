4cc1ec99f0087cec1cd4a945a7c2e8f729992163
Ofer Chen <oferchen@gmail.com>
2025-08-16 22:33:35 +0200

    blockio: drop file mode tracking

--- internal/blockio/blockio.go
+++ internal/blockio/blockio.go
@@ -18,7 +18,6 @@ type File struct {
 	physical int
 	direct   bool
 	strict   bool
-	perm     os.FileMode
 	syncFunc func() error
 }
 
@@ -26,15 +25,14 @@ type File struct {
 // When strict is false and a later write is not block aligned, the file
 // reopens without O_DIRECT to allow buffered I/O.
 func Open(path string, direct, strict bool) (*File, error) {
-	fi, err := os.Stat(path)
-	if err != nil {
+	var err error
+	if _, err = os.Stat(path); err != nil {
 		return nil, fmt.Errorf("stat %q: %w", path, err)
 	}
-	perm := fi.Mode().Perm()
 	logical, physical := detectBlockSizes(path)
 	var fd int
 	if direct {
-		fd, err = unix.Open(path, unix.O_RDWR|unix.O_DIRECT, uint32(perm))
+		fd, err = unix.Open(path, unix.O_RDWR|unix.O_DIRECT, 0)
 		if err != nil {
 			if errors.Is(err, unix.EINVAL) && !strict {
 				direct = false
@@ -47,12 +45,12 @@ func Open(path string, direct, strict bool) (*File, error) {
 	if direct && err == nil {
 		f = os.NewFile(uintptr(fd), path)
 	} else {
-		f, err = os.OpenFile(path, os.O_RDWR, perm)
+		f, err = os.OpenFile(path, os.O_RDWR, 0)
 		if err != nil {
 			return nil, fmt.Errorf("open %q: %w", path, err)
 		}
 	}
-	bf := &File{f: f, logical: logical, physical: physical, direct: direct, strict: strict, perm: perm}
+	bf := &File{f: f, logical: logical, physical: physical, direct: direct, strict: strict}
 	bf.syncFunc = f.Sync
 	return bf, nil
 }
@@ -105,7 +103,7 @@ func (f *File) Sync() error {
 }
 
 func (f *File) reopen() error {
-	nf, err := os.OpenFile(f.f.Name(), os.O_RDWR, f.perm)
+	nf, err := os.OpenFile(f.f.Name(), os.O_RDWR, 0)
 	if err != nil {
 		return fmt.Errorf("open %q: %w", f.f.Name(), err)
 	}
