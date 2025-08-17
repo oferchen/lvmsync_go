d0a80f820434781211dbed99a94125c0aff3cfcf
Ofer Chen <oferchen@gmail.com>
2025-08-17 18:53:12 +0200

    refactor: extract transport test helpers

---
+++ transport/testutil/testutil.go
@@ -0,0 +1,108 @@
+package testutil
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"math/big"
+	"net"
+	"testing"
+	"time"
+
+	"go.uber.org/zap"
+
+	"lvmsync_go/common"
+	"lvmsync_go/transport"
+)
+
+func GenerateSelfSignedCert(t *testing.T) (tls.Certificate, *x509.CertPool) {
+	t.Helper()
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatalf("generate key: %v", err)
+	}
+	tmpl := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(time.Hour),
+		IPAddresses:  []net.IP{net.ParseIP("127.0.0.1")},
+	}
+	der, err := x509.CreateCertificate(rand.Reader, &tmpl, &tmpl, &key.PublicKey, key)
+	if err != nil {
+		t.Fatalf("create cert: %v", err)
+	}
+	cert := tls.Certificate{Certificate: [][]byte{der}, PrivateKey: key}
+	parsed, err := x509.ParseCertificate(der)
+	if err != nil {
+		t.Fatalf("parse cert: %v", err)
+	}
+	pool := x509.NewCertPool()
+	pool.AddCert(parsed)
+	return cert, pool
+}
+
+func NewTransport(t *testing.T, name string) transport.Interface {
+	t.Helper()
+	cfg := transport.Config{Logger: zap.NewNop()}
+	if name == "tcp+tls" || name == "quic" || name == "h2" {
+		cert, pool := GenerateSelfSignedCert(t)
+		cfg.ClientCert = cert
+		cfg.ServerCert = cert
+		cfg.Roots = pool
+	}
+	if name == "ssh" {
+		cfg.SSHUser = "test"
+		cfg.SSHPassword = "pass"
+		cfg.AllowInsecure = true
+	}
+	tr, err := transport.Get(name, cfg)
+	if err != nil {
+		t.Fatalf("get transport %s: %v", name, err)
+	}
+	return tr
+}
+
+type Result struct {
+	Peer common.Handshake
+	Err  error
+}
+
+func RunNegotiation(t *testing.T, tr transport.Interface, serverHS, clientHS common.Handshake) (Result, Result) {
+	t.Helper()
+	ctx := context.Background()
+	ln, err := tr.Listen(ctx, "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("listen: %v", err)
+	}
+	defer ln.Close()
+	addr := ln.Addr().String()
+	srvCh := make(chan Result)
+	done := make(chan struct{})
+	go func() {
+		conn, err := ln.Accept()
+		if err != nil {
+			srvCh <- Result{Err: err}
+			return
+		}
+		peer, err := tr.Negotiate(ctx, conn, transport.Server, serverHS)
+		if err != nil {
+			conn.Close()
+			srvCh <- Result{Peer: peer, Err: err}
+			return
+		}
+		<-done
+		conn.Close()
+		srvCh <- Result{Peer: peer, Err: err}
+	}()
+	conn, err := tr.Dial(ctx, addr)
+	if err != nil {
+		t.Fatalf("dial: %v", err)
+	}
+	peer, err := tr.Negotiate(ctx, conn, transport.Client, clientHS)
+	conn.Close()
+	close(done)
+	srvRes := <-srvCh
+	return srvRes, Result{Peer: peer, Err: err}
+}
