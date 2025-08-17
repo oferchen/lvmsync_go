1ba375343b7b4526cf8c96ac4623cb29777f242c
Ofer Chen <oferchen@gmail.com>
2025-08-17 18:05:27 +0200

    Restrict TLS cipher suites and add negotiation tests (#869)

--- transport/tcp_tls/tcp_tls.go
+++ transport/tcp_tls/tcp_tls.go
@@ -46,15 +46,17 @@ func New(cfg transport.Config) (transport.Interface, error) {
 		clientAuth = tls.RequireAnyClientCert
 	}
 	serverConf := &tls.Config{
-		ClientCAs:  cfg.Roots,
-		ClientAuth: clientAuth,
-		MinVersion: tls.VersionTLS13,
-		NextProtos: []string{alpn},
+		ClientCAs:    cfg.Roots,
+		ClientAuth:   clientAuth,
+		MinVersion:   tls.VersionTLS13,
+		CipherSuites: []uint16{tls.TLS_AES_128_GCM_SHA256, tls.TLS_AES_256_GCM_SHA384, tls.TLS_CHACHA20_POLY1305_SHA256},
+		NextProtos:   []string{alpn},
 	}
 	clientConf := &tls.Config{
 		RootCAs:            cfg.Roots,
 		InsecureSkipVerify: cfg.AllowInsecure,
 		MinVersion:         tls.VersionTLS13,
+		CipherSuites:       []uint16{tls.TLS_AES_128_GCM_SHA256, tls.TLS_AES_256_GCM_SHA384, tls.TLS_CHACHA20_POLY1305_SHA256},
 		NextProtos:         []string{alpn},
 	}
 	if len(cert.Certificate) > 0 {
