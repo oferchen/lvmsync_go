2b598317397921b0a4cf7b32174246ef741f2de9
Ofer Chen <oferchen@gmail.com>
2025-08-17 20:55:04 +0200

    transport: handle nil transports (#905)

--- transport/tcp_tls/tcp_tls.go
+++ transport/tcp_tls/tcp_tls.go
@@ -67,7 +67,16 @@ func New(cfg transport.Config) (transport.Interface, error) {
 }
 
 func init() {
-	transport.MustRegister("tcp+tls", New)
+	transport.MustRegister("tcp+tls", func(cfg transport.Config) (transport.Interface, error) {
+		tr, err := New(cfg)
+		if err != nil {
+			return nil, err
+		}
+		if tr == nil {
+			return nil, fmt.Errorf("tcp+tls: nil transport")
+		}
+		return tr, nil
+	})
 }
 
 func (t *Transport) Name() string { return "tcp+tls" }
