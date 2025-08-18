1e7d64f85f72e48cd1aa18b70fcd4377e779a328
Ofer Chen <oferchen@gmail.com>
2025-08-18 15:07:07 +0200

    Deprecate serve command in favor of lvmsyncd (#980)

---
+++ cmd/serve/serve.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+const deprecationMsg = "serve command deprecated; use lvmsyncd instead"
+
+func newCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "serve",
+		Short: "Deprecated server command",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			return fmt.Errorf(deprecationMsg)
+		},
+		SilenceUsage:  true,
+		SilenceErrors: true,
+	}
+	cmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+		fmt.Fprintln(cmd.OutOrStdout(), deprecationMsg)
+	})
+	return cmd
+}
+
+func run(args []string, w io.Writer) int {
+	cmd := newCommand()
+	cmd.SetArgs(args)
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(w, deprecationMsg)
+		return 1
+	}
+	return 0
+}
+
+func main() {
+	os.Exit(run(os.Args[1:], os.Stderr))
+}
