4e42420309fb1d9ad36595c07fc7e9088807e4e9
Ofer Chen <oferchen@gmail.com>
2025-08-19 14:30:34 +0200

    Add compressed resume integration test (#1150)

---
+++ integration/framed_resume_test.go
@@ -0,0 +1,17 @@
+//go:build integration
+
+package integration
+
+import (
+	"os/exec"
+	"testing"
+)
+
+func TestFramedResumeWithCompression(t *testing.T) {
+	cmd := exec.Command("bash", "./integration/framed_resume.sh")
+	cmd.Dir = ".."
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("framed resume integration failed: %v\n%s", err, out)
+	}
+}
