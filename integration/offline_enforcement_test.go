7c99b60c608c08a0357980d4b0400d77776e55e5
Ofer Chen <oferchen@gmail.com>
2025-08-19 09:54:56 +0200

    Add offline enforcement integration test (#1107)

---
+++ integration/offline_enforcement_test.go
@@ -0,0 +1,17 @@
+//go:build integration
+
+package integration
+
+import (
+	"os/exec"
+	"testing"
+)
+
+func TestOfflineEnforcement(t *testing.T) {
+	cmd := exec.Command("bash", "./integration/offline_enforcement.sh")
+	cmd.Dir = ".."
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("offline enforcement integration failed: %v\n%s", err, out)
+	}
+}
